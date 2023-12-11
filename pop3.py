542bf4f00a8af79eaa11be5954b5d13160d35ef3
Maito_SuperVV <vinh.vader2026@gmail.com>
2023-12-11 20:26:58 +0700

    Determine path to save msg

--- pop3.py
+++ pop3.py
@@ -2,6 +2,9 @@ from socket import *
 import os
 import re
 import json
+from email import message_from_string
+import email
+BUFFER_SIZE=1024
 
 parent_path="D:/Python_Store"
 
@@ -11,6 +14,18 @@ def Check_Exist_msg(msg_name,folder_path):
         if msg_path.exist():
             return True
     return False
+
+def receive_rawEmail(clientSocket):
+    raw_email=b''
+    while True:
+        data_temp=clientSocket.recv(BUFFER_SIZE)
+        raw_email+=data_temp
+        if b'\r\n.\r\n' in data_temp:
+            break       
+    raw_email=raw_email.decode("utf8")
+    pos_first_n=raw_email.find("\r\n")+len("\r\n")
+    raw_email = raw_email[pos_first_n:]
+    return raw_email
 
 def determine_Path(froM, subject, content, msg_name, pair_from, pair_subject, pair_content, pair_spam):
     if froM in pair_from_folder[0]:
@@ -29,30 +44,35 @@ def determine_Path(froM, subject, content, msg_name, pair_from, pair_subject, pa
             path=os.path.join("D:/Python_Store",pair_spam[1])
             return os.path.join(path,msg_name)
     return os.path.join("D:/Python_Store/Inbox",msg_name)
-    #content
-    #spam
+
+def Get_TextBody_msg(msg):
+    if not msg.is_multipart():
+        return msg.get_payload(decode=True).decode(msg.get_content_charset(),'ignore')
+    for part in msg.walk():
+        Content_Type=part.get_content_type()
+        if Content_Type=='text/plain':
+            return part.get_payload(decode=True).decode(part.get_content_charset(),'ignore')
 
 def Download_msgFile(order,msg_name,folder_path,clientSocket,pair_from,pair_subject,pair_content,pair_spam):
-    clientSocket.sendall("RETR {}".format_map(order).encode())
-    boundary=clientSocket.recv(1024).decode()
-    mime_version=clientSocket.recv(1024).decode()
-    msg_id=clientSocket.recv(1024).decode()
-    date=clientSocket.recv(1024).decode()
-    user_agent=clientSocket.recv(1024).decode()
-    to=clientSocket.recv(1024).decode()
-    froM=re.search(r'<([^>]+)>',clientSocket.recv(1024).decode()).group(1)
-    subject=clientSocket.recv(1024).decode()[10:] #? check \r\n in last
-    content="msg"
+    clientSocket.sendall("RETR {}\r\n".format_map(order).encode())
+    raw_email=receive_rawEmail(clientSocket)
+    msg=message_from_string(raw_email)
+    froM=msg.get("From")
+    subject=msg.get("Subject")
+    content=Get_TextBody_msg(msg)
     path_To_save=determine_Path(froM,subject,content,msg_name,pair_from,pair_subject,pair_content,pair_spam)
+    file=open(path_To_save,'r')
+    file.read(raw_email)
+    file.close()
 
-def Get_folder_path():
+def Get_folder_path(parent_path,pair_from_folder,pair_subject_folder,pair_content_folder,pair_spam_folder):
     folder_path=[]
     folder_path.append(os.path.join(parent_path,pair_from_folder[1]))
     folder_path.append(os.path.join(parent_path,pair_subject_folder[1]))
     folder_path.append(os.path.join(parent_path,pair_content_folder[1]))
     folder_path.append(os.path.join(parent_path,pair_spam_folder[1]))
     return folder_path
-
+#Read File Config Start
 f=open("D:/Python_Store/pop3_client/filter_Congif.json","r")
 filters=json.load(f)['Filter']
 pair_from_folder=(filters[0]["From"],filters[0]["ToFolder"])
@@ -60,23 +80,24 @@ pair_subject_folder=(filters[1]["Subject"],filters[1]["ToFolder"])
 pair_content_folder=(filters[2]["Content"],filters[2]["ToFolder"])
 pair_spam_folder=(filters[3]["Spam"],filters[3]["ToFolder"])
 f.close()
+#Read File Config End
 folder_paths=Get_folder_path(parent_path,pair_from_folder[1],pair_subject_folder[1],pair_content_folder[1],pair_spam_folder[1])
 user_email="nqvinhdongthap322004@gmail.com"
 user_pass="vinhdeptrai"
 clientSocket=socket(AF_INET,SOCK_STREAM)
-clientSocket.recv()
-clientSocket.sendall("CAPA".encode())
-clientSocket.recv()
-clientSocket.sendall("USER {}".format(user_email).encode())
-clientSocket.recv()
-clientSocket.sendall("PASS {}".format(user_pass).encode())
-clientSocket.recv()
-clientSocket.sendall("STAT".encode())
-rev1=clientSocket.recv(1024).decode()
+clientSocket.recv(BUFFER_SIZE)
+clientSocket.sendall("CAPA\r\n".encode())
+clientSocket.recv(BUFFER_SIZE)
+clientSocket.sendall("USER {}\r\n".format(user_email).encode())
+clientSocket.recv(BUFFER_SIZE)
+clientSocket.sendall("PASS {}\r\n".format(user_pass).encode())
+clientSocket.recv(BUFFER_SIZE)
+clientSocket.sendall("STAT\r\n".encode())
+rev1=clientSocket.recv(BUFFER_SIZE).decode()
 if(rev1[:5]=='OK 0 0'):
-    clientSocket.send("QUIT".encode())
+    clientSocket.send("QUIT\r\n".encode())
     clientSocket.close()
-clientSocket.sendall("LIST".encode())
+clientSocket.sendall("LIST\r\n".encode())
 clientSocket.recv(1024) #OK
 while (rev := clientSocket.recv(1024)) != b'.':
     msg_name=rev[2:]
@@ -84,6 +105,5 @@ while (rev := clientSocket.recv(1024)) != b'.':
     if Check_Exist_msg(msg_name,folder_paths,clientSocket)==False:
         Download_msgFile(msg_name,folder_paths)
 
-
-clientSocket.send("QUIT".encode())
+clientSocket.send("QUIT\r\n".encode())
 clientSocket.close()
